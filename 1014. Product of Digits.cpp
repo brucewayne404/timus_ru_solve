e20c151b21ce1c9c14dc5e59178c667eec281249
Maruf Hossain <115802127+brucewayne404@users.noreply.github.com>
2023-07-29 15:43:29 +0600

    Create 1014. Product of Digits.cpp

---
+++ 1014. Product of Digits.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    long long n, i, j, m = 0, sum = 0;
+    cin >> n;
+    if(n == 0){
+        cout << 0;
+        return 0;
+    }
+    if(n == 1){
+        cout << 1;
+        return 0;
+    }
+    for(i = 9; i > 1; i--){
+        while(n%i == 0){
+            n = n/i;
+            sum = sum + i*pow(10, m);
+            m++;
+        }
+    }
+    if(n > 1){
+        cout << -1 << endl;
+    }
+    else cout << sum;
+}
